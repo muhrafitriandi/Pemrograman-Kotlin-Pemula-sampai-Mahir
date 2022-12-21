fee724ad549debf024bf750b41dc5d6bc4d33b66
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-21 18:22:29 +0700

    String Data Type
    TrimIndent, String Template

---
+++ Section 1 - Kotlin Dasar/Kotlin-Dasar/src/main/kotlin/tipe_data_string/String.kt
@@ -0,0 +1,19 @@
+package tipe_data_string
+
+fun main() {
+    val firstName = "Muhammad Rafi"
+    val lastName = "Triandi"
+    val address = """
+        Menara Yarsi, Jl. Letjen Suprapto No.Kav. 13,
+        RT.10/RW.5, Cemp. Putih Tim., Kec. Cemp. Putih, Kota Jakarta
+        Pusat, Daerah Khusus Ibukota Jakarta 10510
+    """.trimIndent()
+
+    val fullName = "$firstName $lastName"
+
+    println("Firstname: $firstName")
+    println("Lastname: $lastName")
+    println("Address: $address")
+    println("Fullname: $fullName")
+    println("Length Fullname: ${fullName.length}")
+}
