46314e93a2c7ba9e47d501edc73fc75e6b2ee074
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-22 15:40:31 +0700

    Comparison Operator

---
+++ Section 1 - Kotlin Dasar/Kotlin-Dasar/src/main/kotlin/operasi_perbandingan/ComparisonOperator.kt
@@ -0,0 +1,12 @@
+package operasi_perbandingan
+
+fun main() {
+    println(1 > 2) // false
+    println(1 < 2) // true
+    println(1 >= 2) // false
+    println(1 <= 2) // true
+    println(1 == 2) // false
+    println(1 != 2) // true
+
+    println("b" > "a") // true
+}
