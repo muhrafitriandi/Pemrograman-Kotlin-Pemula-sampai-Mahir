8bc7cddac5c5ad1539b63546c0d99def4f474cec
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-25 21:10:00 +0700

    Specifify return type explicitly

--- Section 1 - Kotlin Dasar/Kotlin-Dasar/src/main/kotlin/single_expression_function/SingleExpressionFunction.kt
+++ Section 1 - Kotlin Dasar/Kotlin-Dasar/src/main/kotlin/single_expression_function/SingleExpressionFunction.kt
@@ -1,7 +1,7 @@
 package single_expression_function
 
-fun calculateArea(length: Int, width: Int) = length * width
-fun sayHello(name: String) = println("Hello $name")
+fun calculateArea(length: Int, width: Int): Int = length * width
+fun sayHello(name: String): Unit = println("Hello $name")
 fun main() {
     sayHello("Rafi")
     println("${calculateArea(6, 2)} cm")
