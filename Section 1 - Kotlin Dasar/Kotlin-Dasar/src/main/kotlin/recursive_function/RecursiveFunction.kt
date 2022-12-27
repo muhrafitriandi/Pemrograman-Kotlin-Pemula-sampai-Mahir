3e9199bfe9d0803db3119c2aa611acd221c95db4
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-28 01:22:00 +0700

    Recursive Function with Independent Challenge
    Fibonacci using Recursive Function

--- Section 1 - Kotlin Dasar/Kotlin-Dasar/src/main/kotlin/recursive_function/RecursiveFunction.kt
+++ Section 1 - Kotlin Dasar/Kotlin-Dasar/src/main/kotlin/recursive_function/RecursiveFunction.kt
@@ -18,7 +18,32 @@ fun factorialRecursive(n: Int): Int {
     }
 }
 
+// Independent Challenge
+
+fun fibonacciLoop(n: Int): Int {
+    var fn = 0
+    var fn1 = 1
+    var fn2 = 0
+    for (i in 1 until n) {
+        fn = fn1 + fn2
+        fn2 = fn1
+        fn1 = fn
+    }
+    return fn
+}
+
+fun fibonacciRecursive(n: Int): Int {
+    return when (n) {
+        0 -> 0
+        1 -> 1
+        else -> fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
+    }
+}
+
 fun main() {
     println(factorialLoop(5))
     println(factorialRecursive(5))
+
+    println(fibonacciLoop(10))
+    println(fibonacciRecursive(10))
 }
