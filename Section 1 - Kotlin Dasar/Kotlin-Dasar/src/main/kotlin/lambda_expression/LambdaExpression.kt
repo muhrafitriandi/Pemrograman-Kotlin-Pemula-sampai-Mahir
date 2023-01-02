794f1fb51ffe33dd49248c372e703df7866ec6a9
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-02 23:44:14 +0700

    Lambda Expression
    Variable, It, Method References, Parameter Function

---
+++ Section 1 - Kotlin Dasar/Kotlin-Dasar/src/main/kotlin/lambda_expression/LambdaExpression.kt
@@ -0,0 +1,23 @@
+package lambda_expression
+
+fun main() {
+    // Lambda Expression in Variable with 2 Parameters
+    val sum = { x: Int, y: Int -> x.plus(y) }
+    println(sum(1, 2))
+
+    // Lambda Expression It (It represents the first parameter (the object being processed))
+    val gender: (String) -> String = { it.uppercase() }
+    println(gender("Male"))
+
+    // Lambda Expression, Method References
+    val multiplication = ::multiplication
+    println(multiplication(2, 3))
+
+    // Lambda Expression, Parameter Function
+    val result = processNumbers(2, 5) { x, y -> x.times(y) }
+    println(result)
+}
+
+fun processNumbers(x: Int, y: Int, action: (Int, Int) -> Int): Int = action(x, y)
+
+fun multiplication(x: Int, y: Int): Int = x.times(y)
