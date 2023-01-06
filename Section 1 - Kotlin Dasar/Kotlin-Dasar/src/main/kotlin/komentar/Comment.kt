9d1e744e3bc9a7cf6551a475128db6d1c9d94a2a
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-06 22:45:54 +0700

    Komentar
    Single Line, Multi Line Comment

---
+++ Section 1 - Kotlin Dasar/Kotlin-Dasar/src/main/kotlin/komentar/Comment.kt
@@ -0,0 +1,11 @@
+package komentar
+
+/**
+ * This function will sum the two numbers.
+ * @param 1 and 2 are numbers with Int data type.
+ * @return Int value of the sum.
+ */
+fun sum(x: Int, y: Int): Int = x.plus(y)
+fun main() {
+    println(sum(5, 5)) // call sum function
+}
