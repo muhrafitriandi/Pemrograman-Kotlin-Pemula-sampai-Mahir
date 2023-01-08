51940976af887f24e0b5dc4ea3c751aca9cf4856
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-08 23:47:36 +0700

    Object

---
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/object/Object.kt
@@ -0,0 +1,15 @@
+package `object`
+
+import kelas.Cat
+import kelas.Dog
+import kelas.Horse
+
+fun main() {
+    val felix = Dog()
+    val aleta = Dog()
+    val chiyo = Cat()
+    val bao = Cat()
+    val roxy = Horse()
+    val trixie = Horse()
+}
+
