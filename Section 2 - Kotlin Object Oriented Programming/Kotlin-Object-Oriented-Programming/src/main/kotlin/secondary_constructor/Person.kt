e9064c1df74c126497c1651f9957e2158423888b
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-10 07:11:11 +0700

    Secondary Constructor without Primary Constructor

--- Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/secondary_constructor/Person.kt
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/secondary_constructor/Person.kt
@@ -14,3 +14,31 @@ class Person(name: String, age: Int, address: String) {
         println("Constructor 2")
     }
 }
+
+// Without Primary Constructor
+//class Person {
+//    var name: String = ""
+//    var age: Int = 0
+//    var address: String = ""
+//    constructor(nameParam: String, ageParam: Int, addressParam: String) {
+//        name = nameParam
+//        age = ageParam
+//        address = addressParam
+//        println("Constructor 1")
+//    }
+//
+//    constructor(nameParam: String, ageParam: Int) {
+//        name = nameParam
+//        age = ageParam
+//        println("Constructor 2")
+//    }
+//
+//    constructor(nameParam: String) {
+//        name = nameParam
+//        println("Constructor 3")
+//    }
+//}
+
+// Note:
+// Without Primary Constructor, it is not necessary to use this (to execute other constructors).
+// Without Primary Constructor, the code will look messier, because we have to declare variables and give default values which are eventually replaced with the parameters in the secondary constructor.
