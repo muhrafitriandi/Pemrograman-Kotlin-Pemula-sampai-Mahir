134a957ef782f0239fdf6477f4efbb76bced5f69
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-10 07:24:32 +0700

    Properties Constructor

---
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/properties_di_constructor/Person.kt
@@ -0,0 +1,3 @@
+package properties_di_constructor
+
+class Person(val name: String, val age: Int, var address: String)
---
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/properties_di_constructor/PropertiesConstrictor.kt
@@ -0,0 +1,8 @@
+package properties_di_constructor
+
+fun main() {
+    val rafi = Person("Rafi", 22, "Jl Kenangan No.12A")
+    println(rafi.name)
+    println(rafi.age)
+    println(rafi.address)
+}
