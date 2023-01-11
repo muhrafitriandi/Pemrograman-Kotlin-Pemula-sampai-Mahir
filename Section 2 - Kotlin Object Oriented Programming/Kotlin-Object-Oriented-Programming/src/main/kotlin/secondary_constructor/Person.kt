e17bde6d6b8bdc010ef57654e80fd84a60cb4a08
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-12 03:31:29 +0700

    Get Properties Constructor

--- Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/secondary_constructor/Person.kt
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/secondary_constructor/Person.kt
@@ -1,12 +1,12 @@
 package secondary_constructor
 
-class Person(name: String, age: Int, address: String) {
+class Person(val name: String, val age: Int, val address: String) {
 
     init {
         println("Person $name created")
     }
 
-    constructor(name: String, age: Int): this(name, age, "") {
+    constructor(name: String, age: Int): this(name, age, "xxx") {
         println("Constructor 1")
     }
 
--- Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/secondary_constructor/SecondaryConstructor.kt
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/secondary_constructor/SecondaryConstructor.kt
@@ -2,6 +2,17 @@ package secondary_constructor
 
 fun main() {
     val rafi = Person("Rafi")
+    println(rafi.name)
+    println(rafi.age)
+    println(rafi.address)
+
     val madun = Person("Madun", 22)
+    println(madun.name)
+    println(madun.age)
+    println(madun.address)
+
     val rizal = Person("Rizal", 24, "Jl. Kenanga No.12A")
+    println(rizal.name)
+    println(rizal.age)
+    println(rizal.address)
 }
