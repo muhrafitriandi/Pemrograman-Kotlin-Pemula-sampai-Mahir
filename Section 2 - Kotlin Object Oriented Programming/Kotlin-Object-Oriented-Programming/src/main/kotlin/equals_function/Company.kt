1fc8ac79e9f9372d355d5976ca9192a4ba9b874c
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-12 22:25:16 +0700

    Equals Function

---
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/equals_function/Company.kt
@@ -0,0 +1,10 @@
+package equals_function
+
+class Company(val name: String) {
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is Company -> other.name == this.name
+            else -> false
+        }
+    }
+}
---
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/equals_function/EqualsFunction.kt
@@ -0,0 +1,8 @@
+package equals_function
+
+fun main() {
+    val company1 = Company("Grab")
+    val company2 = Company("Grab")
+
+    println(company1 == company2)
+}
