fc588375f976ef204642a60cde327f12636fbd37
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-13 20:18:00 +0700

    Interface

---
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/interfaces/Driveable.kt
@@ -0,0 +1,39 @@
+package interfaces
+
+interface Driveable {
+    val name: String
+    fun startEngine()
+    fun stopEngine()
+    fun drive()
+
+}
+
+class Car(override val name: String) : Driveable {
+    override fun startEngine() {
+        println("Start Engine ${this.name} Car")
+    }
+
+    override fun stopEngine() {
+        println("Stop Engine ${this.name} Car")
+    }
+
+    override fun drive() {
+        println("Driving ${this.name} Car")
+    }
+
+}
+
+class Motorcycle(override val name: String) : Driveable {
+    override fun startEngine() {
+        println("Start Engine ${this.name} Motorcycle")
+    }
+
+    override fun stopEngine() {
+        println("Stop Engine ${this.name} Motorcycle")
+    }
+
+    override fun drive() {
+        println("Driving ${this.name} Motorcycle")
+    }
+
+}
---
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/interfaces/Interface.kt
@@ -0,0 +1,9 @@
+package interfaces
+
+fun main() {
+    val car = Car("Avanza")
+    val motorcycle = Motorcycle("Vario")
+
+    car.drive()
+    motorcycle.drive()
+}
