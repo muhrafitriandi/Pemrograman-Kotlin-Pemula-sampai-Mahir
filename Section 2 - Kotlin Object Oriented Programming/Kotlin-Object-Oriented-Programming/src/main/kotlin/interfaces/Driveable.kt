921d13310fc0aec1d9e24fa8f11e850d674317ab
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-13 20:35:12 +0700

    Inheritance Interface

--- Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/interfaces/Driveable.kt
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/interfaces/Driveable.kt
@@ -1,20 +1,24 @@
 package interfaces
 
-interface Vehicle {
-    val name: String
+interface Engine {
     fun startEngine()
     fun stopEngine()
+}
+
+interface Vehicle : Engine {
+    val name: String
+
     fun honk() {
         println("Honk Honk")
     }
 }
 
-interface Driveable {
+interface Driveable : Vehicle {
     fun drive()
 }
 
 
-class Car(override val name: String) : Vehicle, Driveable {
+class Car(override val name: String) : Driveable {
     override fun startEngine() {
         println("Start Engine ${this.name} Car")
     }
@@ -29,7 +33,7 @@ class Car(override val name: String) : Vehicle, Driveable {
 
 }
 
-class Motorcycle(override val name: String) : Vehicle, Driveable {
+class Motorcycle(override val name: String) : Driveable {
     override fun startEngine() {
         println("Start Engine ${this.name} Motorcycle")
     }
