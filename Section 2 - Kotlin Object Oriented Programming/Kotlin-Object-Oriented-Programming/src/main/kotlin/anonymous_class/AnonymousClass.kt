fdc5e79dcf6d60f5651a30762ab29b6c97936d5c
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-15 04:43:20 +0700

    Anonymous Class

---
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/anonymous_class/AnonymousClass.kt
@@ -0,0 +1,25 @@
+package anonymous_class
+
+fun attack(attack: IAttack) {
+    attack.cubit()
+}
+
+fun main() {
+    // Cara umum
+    val balmond = Balmond()
+    attack(balmond)
+
+    // Gunakan anonymous class jika objek tidak memiliki properties, daripada harus membuat object dan class yang mengimplementasikannya secara berulang-ulang
+    // Jika memiliki properties, gunakanlah abstract class agar menjadi lebih dinamis
+    attack(object : IAttack {
+        override fun cubit() {
+            println("Balmond Nyubit menggunakan jepitan jemuran")
+        }
+    })
+
+    attack(object : IAttack {
+        override fun cubit() {
+            println("Layla Nyubit menggunakan gunting")
+        }
+    })
+}
---
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/anonymous_class/IAttack.kt
@@ -0,0 +1,12 @@
+package anonymous_class
+
+interface IAttack {
+    fun cubit()
+}
+
+class Balmond : IAttack {
+    override fun cubit() {
+        println("Balmond Nyubit menggunakan jepitan jemuran")
+    }
+
+}
