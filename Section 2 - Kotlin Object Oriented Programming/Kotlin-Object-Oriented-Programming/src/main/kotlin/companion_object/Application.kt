baa2b465a39f703e92fc1f6da55d2058e9b5883b
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-16 00:16:35 +0700

    Companion Object

---
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/companion_object/Application.kt
@@ -0,0 +1,9 @@
+package companion_object
+
+class Application {
+    companion object {
+        fun version(name: String) {
+            println("Version: $name")
+        }
+    }
+}
---
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/companion_object/CompanionObject.kt
@@ -0,0 +1,6 @@
+package companion_object
+
+fun main() {
+    Application.version("1.0")
+    // Application.Companion.version("1.0")
+}
