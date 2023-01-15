ce5b656eb04635e5441d01354c19056c0128d812
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-16 01:31:16 +0700

    Type Alias Function

--- Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/type_alias/TypeAlias.kt
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/type_alias/TypeAlias.kt
@@ -5,10 +5,23 @@ import type_alias.test1.Application
 typealias App1 = Application
 typealias App2 = type_alias.test2.Application
 
+typealias Count = (Int, Int) -> Int
+
+fun sum(x: Int, y: Int, count: Count) {
+    println("$x + $y = ${count(x, y)}")
+}
+
+fun subtraction(x: Int, y: Int, count: Count) {
+    println("$x - $y = ${count(x, y)}")
+}
+
 fun main() {
     val app1 = App1
     val app2 = App2
 
     app1.version("1.2")
     app2.version("1.3")
+
+    sum(10, 5, Int::plus)
+    subtraction(10, 5, Int::minus)
 }
