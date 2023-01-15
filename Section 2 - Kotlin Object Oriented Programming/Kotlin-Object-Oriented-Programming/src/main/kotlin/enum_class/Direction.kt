8c7768c651750bbd1ab92d59273bc0b364e3eb03
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-15 07:17:21 +0700

    Properties & Function Enum Class

--- Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/enum_class/Direction.kt
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/enum_class/Direction.kt
@@ -3,3 +3,14 @@ package enum_class
 enum class Direction {
     NORTH, SOUTH, EAST, WEST
 }
+
+//enum class Direction(val description: String) {
+//    NORTH("North"),
+//    SOUTH("South"),
+//    EAST("East"),
+//    WEST("West");
+//
+//    fun showDescription() {
+//        println(description)
+//    }
+//}
--- Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/enum_class/EnumClass.kt
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/enum_class/EnumClass.kt
@@ -4,4 +4,6 @@ fun main() {
     val currentDirection = Direction.NORTH
     println(currentDirection)
     println(Direction.values().toList())
+
+    // Direction.values().forEach(Direction::showDescription)
 }
