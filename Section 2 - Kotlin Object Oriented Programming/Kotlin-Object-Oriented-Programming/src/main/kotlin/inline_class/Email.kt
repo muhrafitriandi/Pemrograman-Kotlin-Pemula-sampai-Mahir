015f422939b06b8db245516f57f532a352290e5e
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-16 02:36:32 +0700

    Inner Class

--- Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/inline_class/Email.kt
+++
@@ -1,8 +0,0 @@
-package inline_class
-
-@JvmInline
-value class Email(val value: String) {
-    fun toUpper(): String = this.value.uppercase()
-}
-
-// inline class deprecated: using @JvmInline value
--- Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/inline_class/InlineClass.kt
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/inline_class/InlineClass.kt
@@ -1,5 +1,11 @@
 package inline_class
 
+@JvmInline
+value class Email(val value: String) {
+    fun toUpper(): String = this.value.uppercase()
+}
+// inline class deprecated: using @JvmInline value
+
 fun main() {
     val email = Email("example@example.com")
     println(email.value)
