52847652c025d4e784ef55fb41ffa990e826f8f5
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-20 00:00:40 +0700

    Multiple Catch

--- Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/exception/Exception.kt
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/exception/Exception.kt
@@ -1,19 +1,24 @@
 package exception
 
-fun validatePassword(password: String) {
-    if (password.length <= 8) {
-        throw InvalidPasswordException("Password is too short")
-    } else {
-        println("Your password is $password")
+fun validatePassword(password: String?) {
+    password!!.let {
+        if (password.length <= 8) {
+            throw InvalidPasswordException("Password is too short")
+        } else {
+            println("Your password is $password")
+        }
     }
 }
 
 fun main() {
     try {
         validatePassword("Sl4met4lonso45")
-        validatePassword("sl4met") // Ini akan error, ketika error maka...
+        validatePassword(null) // Ini akan error, ketika error maka...
+        // validatePassword("sl4met") // Ini akan error, ketika error maka...
         println("Testing") // Statement ini tidak akan dijalankan.
     } catch (error: InvalidPasswordException) {
         println("Sesuatu telah terjadi: ${error.message}")
+    } catch (error: NullPointerException) {
+        println("Password tidak boleh kosong")
     }
 }
