80061b0aaa765c7614cd3dd8a695f89ee7c19fac
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-19 23:44:30 +0700

    Try Catch

--- Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/exception/Exception.kt
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/exception/Exception.kt
@@ -9,6 +9,11 @@ fun validatePassword(password: String) {
 }
 
 fun main() {
-    validatePassword("Sl4met4lonso45")
-    validatePassword("sl4met")
+    try {
+        validatePassword("Sl4met4lonso45")
+        validatePassword("sl4met") // Ini akan error, ketika error maka...
+        println("Testing") // Statement ini tidak akan dijalankan.
+    } catch (error: InvalidPasswordException) {
+        println("Sesuatu telah terjadi: ${error.message}")
+    }
 }
