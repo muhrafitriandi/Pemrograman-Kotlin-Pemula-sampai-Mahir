d13d7c872d93a590a3565db86da0ec43c22c5511
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-21 23:20:53 +0700

    Generic Multiple Paramater Type

--- Section 3 - Kotlin Generic/Kotlin-Generic/src/main/kotlin/generic_class/GenericClass.kt
+++ Section 3 - Kotlin Generic/Kotlin-Generic/src/main/kotlin/generic_class/GenericClass.kt
@@ -1,11 +1,17 @@
 package generic_class
 
 fun main() {
-    val intValue: MyGenericClass<Int> = MyGenericClass<Int>(100)
-    val stringValue: MyGenericClass<String> = MyGenericClass<String>("Rafi")
+    val intValue = MyGenericClass(100)
+    val stringValue = MyGenericClass("Rafi")
 
     intValue.printData()
     println(intValue.getData()) // will automatically be int without cast
     stringValue.printData()
     println(stringValue.getData()) // will automatically be string without cast
+
+    val intStringPair = Pair(1, "Satu")
+    val stringDoublePair = Pair("Tiga Koma Empat Belas", 3.14)
+
+    intStringPair.printData()
+    stringDoublePair.printData()
 }
--- Section 3 - Kotlin Generic/Kotlin-Generic/src/main/kotlin/generic_class/MyGenericClass.kt
+++ Section 3 - Kotlin Generic/Kotlin-Generic/src/main/kotlin/generic_class/MyGenericClass.kt
@@ -5,3 +5,11 @@ class MyGenericClass<T>(val value: T) {
 
     fun printData() = println("The value is ${this.value}")
 }
+
+class Pair<T, U>(val first: T, val second: U) {
+    fun getFirstData(): T = first
+
+    fun getSecondData(): U = second
+
+    fun printData() = println("First data is ${this.first}. Second data is ${this.second}")
+}
