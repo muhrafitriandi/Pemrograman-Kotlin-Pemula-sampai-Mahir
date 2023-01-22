79c72d8c5b085ec1b5f08a42493c5e0e921b7f19
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-01-22 12:23:11 +0700

    Where Keyword

--- Section 3 - Kotlin Generic/Kotlin-Generic/src/main/kotlin/generic_constraint/Employee.kt
+++ Section 3 - Kotlin Generic/Kotlin-Generic/src/main/kotlin/generic_constraint/Employee.kt
@@ -1,5 +1,9 @@
 package generic_constraint
 
+interface IWalk {
+    fun walk()
+}
+
 open class Employee(val name: String) {
     open fun sayHello(name: String) {
         println("Hello $name, my name is ${this.name}")
@@ -12,10 +16,18 @@ class Manager(name: String) : Employee(name) {
     }
 }
 
-class VicePresident(name: String) : Employee(name) {
+class VicePresident(name: String) : Employee(name), IWalk {
     override fun sayHello(name: String) {
         println("Hello $name, my vice president name is ${this.name}")
     }
+
+    override fun walk() {
+        println("I'm walking")
+    }
 }
 
-class Company<T : Employee>(val employee: T)
+// generic 1 parameter type
+// class Company<T>(val employee: T) where T : Employee, T : IWalk
+
+// generic 2 parameter type
+class Company<T, U>(val employee: T) where T : Employee, U : IWalk
--- Section 3 - Kotlin Generic/Kotlin-Generic/src/main/kotlin/generic_constraint/GenericConstraint.kt
+++ Section 3 - Kotlin Generic/Kotlin-Generic/src/main/kotlin/generic_constraint/GenericConstraint.kt
@@ -1,12 +1,19 @@
 package generic_constraint
 
 fun main() {
-    val data1 = Company(Employee("Rafi"))
-    val data2 = Company(Manager("Jose Mourinho"))
-    val data3 = Company(VicePresident("Amin Maruf"))
+    // with generic 1 parameter type. Jadi, harus mengimplementasikan Employee dan IWalk.
+    // val data1 = Company(Employee("Rafi")) // error: not implemented IWalk
+    // val data2 = Company(Manager("Jose Mourinho")) // error: not implemented IWalk
+
+    // solve: with generic 2 parameter type. Dengan ini, tidak harus mengimplementasikan keduanya.
+    val data1 = Company<Employee, IWalk>(Employee("Rafi"))
+    val data2 = Company<Manager, IWalk>(Manager("Jose Mourinho"))
+
+    val data3 = Company<VicePresident, IWalk>(VicePresident("Amin Maruf"))
     // val data4 = Company("This is a String") // error: type mismatch
 
     data1.employee.sayHello("Ronaldo")
     data2.employee.sayHello("Messi")
     data3.employee.sayHello("Mbappe")
+    data3.employee.walk()
 }
