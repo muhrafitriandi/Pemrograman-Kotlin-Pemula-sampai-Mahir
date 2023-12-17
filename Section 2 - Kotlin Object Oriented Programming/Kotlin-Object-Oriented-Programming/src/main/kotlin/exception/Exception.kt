d1919f4de9c8772e5051f5e983b19e542334d2ca
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-12-17 15:53:12 +0700

    handling multiple value exception

--- Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/exception/Exception.kt
+++ Section 2 - Kotlin Object Oriented Programming/Kotlin-Object-Oriented-Programming/src/main/kotlin/exception/Exception.kt
@@ -2,19 +2,17 @@ package exception
 
 fun validatePassword(password: String?) {
     password!!.let {
-        if (password.length <= 8) {
-            throw InvalidPasswordException("Password is too short")
-        } else {
-            println("Your password is $password")
-        }
+        if (password.isBlank()) throw InvalidPasswordException("Password cannot be empty")
+        if (password.length <= 8) throw InvalidPasswordException("Password is too short")
+        println("Your password is $password")
     }
 }
 
 fun main() {
     try {
         validatePassword("Sl4met4lonso45")
-        validatePassword(null) // Ini akan error, ketika error maka...
-        // validatePassword("sl4met") // Ini akan error, ketika error maka...
+        validatePassword("") // Ini akan error, ketika error maka...
+        validatePassword("sl4met") // Ini akan error, namun statement ini tidak akan dijalankan.
         println("Testing") // Statement ini tidak akan dijalankan.
     } catch (error: InvalidPasswordException) {
         println("Sesuatu telah terjadi: ${error.message}")
