1786167ae15ef7a98a77c5ae165f103a563c2e51
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-01-22 05:26:41 +0700

    Add Case Grouping Interface

---
+++ Section 4 - Kotlin Collection/Kotlin-Collection/src/main/kotlin/grouping_interface/case1/Case1.kt
@@ -0,0 +1,21 @@
+package grouping_interface.case1
+
+fun main() {
+    val numbers = listOf(1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5)
+
+    val numbersGrouped: Grouping<Int, Int> = numbers.groupingBy { it }
+
+    val countNumberBySameValue: Map<Int, Int> = numbersGrouped.eachCount()
+    println(countNumberBySameValue)
+
+    val reduceAndSum: Map<Int, Int> = numbersGrouped.reduce { _, first, second ->
+        first + second
+    }
+    println(reduceAndSum)
+
+    val sumValues1: Int = numbers.reduce { first, second ->
+        first + second
+    }
+    val sumValues2: Int = reduceAndSum.values.sum()
+    println("$sumValues2")
+}
