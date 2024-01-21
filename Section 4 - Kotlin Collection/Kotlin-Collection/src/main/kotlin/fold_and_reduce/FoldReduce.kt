7e0da27b7054153bf94c2cefdf374143e02ff2f6
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-01-22 05:13:09 +0700

    Update Case Fold & Reduce

--- Section 4 - Kotlin Collection/Kotlin-Collection/src/main/kotlin/fold_and_reduce/FoldReduce.kt
+++ Section 4 - Kotlin Collection/Kotlin-Collection/src/main/kotlin/fold_and_reduce/FoldReduce.kt
@@ -2,7 +2,6 @@ package fold_and_reduce
 
 fun main() {
     val numbers = listOf(1, 2, 3, 4, 5)
-    val words = listOf("Kotlin", "is", "awesome")
 
     // Menggunakan reduce untuk menjumlahkan semua elemen dalam list
     val sum = numbers.reduce { first, second ->
@@ -22,31 +21,37 @@ fun main() {
 
     println()
 
-    // Menggunakan reduceRight untuk menggabungkan kata dari kanan ke kiri
-    val reversedString = words.reduceRight { first, second -> "$second $first" }
-    println("Reversed String using reduceRight: $reversedString")
+    // Menggunakan reduceRight untuk menjumlahkan semua elemen dalam list dari kanan ke kiri
+    val sumReductRight = numbers.reduceRight { first, second ->
+        println("$first + $second = ${first + second}")
+        first + second
+    }
+    println("$sumReductRight")
 
     println()
 
-    // Menggunakan foldRight untuk menggabungkan kata dari kanan ke kiri, dengan nilai awal "Text:"
-    val reversedStringWithInitial = words.foldRight("Text:") { first, second -> "$second $first" }
-    println("Reversed String using foldRight with initial value: $reversedStringWithInitial")
+    // Menggunakan foldRight untuk menjumlahkan semua elemen dalam list dari kanan ke kiri, dengan nilai awal 0
+    val sumFoldRight = numbers.foldRight(0) { first, second ->
+        println("$first + $second = ${first + second}")
+        first + second
+    }
+    println("$sumFoldRight")
 
     println()
 
     // Menggunakan reduceIndexed untuk menjumlahkan elemen pada indeks genap
-    val productOfOddIndexed = numbers.reduceIndexed { index, first, second ->
+    val productOfEvenIndexed = numbers.reduceIndexed { index, first, second ->
         if (index % 2 == 0) {
             println("$first + $second = ${first + second}")
             first + second
         } else first
     }
-    println("Product of even indexed elements using reduceIndexed: $productOfOddIndexed")
+    println("Product of even indexed elements using reduceIndexed: $productOfEvenIndexed")
 
     println()
 
-    // Menggunakan foldIndexed untuk menjumlahkan elemen pada indeks genap, dengan nilai awal 1
-    val productOfEvenIndexedWithInitial = numbers.foldIndexed(1) { index, first, second ->
+    // Menggunakan foldIndexed untuk menjumlahkan elemen pada indeks genap, dengan nilai awal 0
+    val productOfEvenIndexedWithInitial = numbers.foldIndexed(0) { index, first, second ->
         if (index % 2 == 0) {
             println("$first + $second = ${first + second}")
             first + second
@@ -56,13 +61,23 @@ fun main() {
 
     println()
 
-    // Menggunakan reduceRightIndexed untuk menggabungkan kata dari kanan ke kiri dengan indeks
-    val reversedStringWithIndex = words.reduceRightIndexed { index, first, second -> "$second $index:$first" }
-    println("Reversed String with index using reduceRightIndexed: $reversedStringWithIndex")
+    // Menggunakan reduceRightIndexed untuk menjumlahkan elemen pada indeks genap, dari kanan ke kiri
+    val productOfEvenReduceRightIndexed = numbers.reduceRightIndexed { index, first, second ->
+        if (index % 2 == 0) {
+            println("$first + $second = ${first + second}")
+            first + second
+        } else first
+    }
+    println("Product of even indexed elements using reduceRightIndexed: $productOfEvenReduceRightIndexed")
 
     println()
 
-    // Menggunakan foldRightIndexed untuk menggabungkan kata dari kanan ke kiri dengan indeks, dengan nilai awal "Text:"
-    val reversedStringWithIndexAndInitial = words.foldRightIndexed("Text:") { index, first, second -> "$second $index:$first" }
-    println("Reversed String with index using foldRightIndexed with initial value: $reversedStringWithIndexAndInitial")
+    // Menggunakan foldRightIndexed untuk menjumlahkan elemen pada indeks genap, dengan nilai awal 0, dari kanan ke kiri
+    val productOfEvenFoldRightIndexed = numbers.foldRightIndexed(0) { index, first, second ->
+        if (index % 2 == 0) {
+            println("$first + $second = ${first + second}")
+            first + second
+        } else first
+    }
+    println("Product of even indexed elements using foldRightIndexed: $productOfEvenFoldRightIndexed")
 }
