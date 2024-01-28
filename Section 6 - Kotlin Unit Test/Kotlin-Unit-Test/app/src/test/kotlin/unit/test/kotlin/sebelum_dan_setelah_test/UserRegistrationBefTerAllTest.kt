73d5905b6010d3683271f9d076804b000be3d514
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-01-28 19:24:05 +0700

    Sebelum & Setelah Test
    - BeforeEach & AfterEach (will be called every time the @Test annotation is run).
    - BeforeAll & AfterAll (only called once before and after the test is run).
    - TestInstance(TestInstance.Lifecycle.PER_CLASS) (to running class-level functions or make them static functions/object functions).
    - TestInstance(TestInstance.Lifecycle.PER_METHOD) (to running instance-level functions or make them non-static functions).

---
+++ Section 6 - Kotlin Unit Test/Kotlin-Unit-Test/app/src/test/kotlin/unit/test/kotlin/sebelum_dan_setelah_test/UserRegistrationBefTerAllTest.kt
@@ -0,0 +1,60 @@
+package unit.test.kotlin.sebelum_dan_setelah_test
+
+import org.junit.jupiter.api.*
+import unit.test.kotlin.menggunakan_assertions.case1.UserRegistration
+import unit.test.kotlin.menggunakan_assertions.case1.data.Register
+import unit.test.kotlin.menggunakan_assertions.case1.exception.ValidationException
+import kotlin.test.*
+import kotlin.test.Test
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UserRegistrationBefTerAllTest {
+    private lateinit var userRegistration: UserRegistration
+
+    @BeforeAll
+    fun setUp() {
+        // Inisialisasi objek atau persiapan lain sebelum semua pengujian dijalankan
+        userRegistration = UserRegistration()
+        println("Set up UserRegistration.")
+    }
+
+    @AfterAll
+    fun tearDown() {
+        // Membersihkan sumber daya atau melakukan tugas pembersihan setelah semua pengujian selesai
+        // Contoh: Menutup koneksi database, membersihkan file sementara, dll.
+        println("Tear down UserRegistration.")
+    }
+
+    @Test
+    fun testRegisterSuccess() {
+        println("testRegisterSuccess")
+        val isSuccess = userRegistration.register(Register("triandy", "Testing193!"))
+        assertTrue(isSuccess, "Registration should be successful.")
+    }
+
+    @Test
+    fun testRegisterBlankUsername() {
+        println("testRegisterBlankUsername")
+        assertThrows<ValidationException> {
+            userRegistration.register(Register("", "Testing193!"))
+        }
+    }
+
+    @Test
+    fun testRegisterWeakPassword() {
+        println("testRegisterWeakPassword")
+        assertThrows<ValidationException> {
+            userRegistration.register(Register("john_doe", "123"))
+        }
+    }
+
+    @Test
+    fun testRegisterDuplicateUsername() {
+        println("testRegisterDuplicateUsername")
+        userRegistration.register(Register("alice", "SecurePassword193!"))
+
+        assertThrows<ValidationException> {
+            userRegistration.register(Register("alice", "NewPassword193!"))
+        }
+    }
+}
